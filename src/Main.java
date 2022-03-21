647ffc132acf2b9b1df16271a9168fd4b03f404c
Kozimjon <kozimkuchkarov77@gmail.com>
2022-03-21 11:05:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+       final int TOTAL_STUDENTS = 10;
+       String[] names = new String[TOTAL_STUDENTS];
+
+       String[] toys = {"Car", "Truck", "Doll"};
+
+
+       names[0] = "Kozimjon";
+       names[1] = "Faru";
+
+       for (int s = 0; s < names.length; s++) {
+           System.out.println(s + " " + names[s]);
+       }
+
+       for (String t:toys)
+       {
+           System.out.println("Toy: " + t);
+       }
+       int[] primes = {1, 2, 3, 5, 7, 11};
+        for (int s = 0; s < primes.length; s++) {
+            System.out.println(s + " " + primes[s]);
+        }
+
+        int sum = 0;
+        double average = 0;
+
+        for(int p : primes)
+        {
+            sum += p; // sum = sum + p
+        }
+        average = (double) sum / primes.length;
+        System.out.println("Average is " + average);
+    }
+}
